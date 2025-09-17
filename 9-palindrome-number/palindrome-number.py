1f34bc5a5ea167e8b159d0666a023c59dbc89255
Samiksha Dnyanoba Poul <poulsamiksha7@gmail.com>
2025-09-17 23:36:31 +0530

    Time: 14 ms (37.67%) | Memory: 12.4 MB (51.66%) - LeetSync

---
+++ 9-palindrome-number/palindrome-number.py
@@ -0,0 +1,16 @@
+class Solution(object):
+    def isPalindrome(self, x):
+        """
+        :type x: int
+        :rtype: bool
+        """
+        original=x
+        rev=0
+        while x>0:
+            digit=x%10
+            rev=rev*10+digit
+            x=x//10
+        if rev==original:
+             return True
+        else:
+                return False
