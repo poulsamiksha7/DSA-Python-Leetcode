5b2f4bfb57ed236835147537a825ff2dd796f26c
Samiksha Dnyanoba Poul <poulsamiksha7@gmail.com>
2025-09-25 08:38:43 +0530

    Time: 15 ms (65.43%) | Memory: 12.8 MB (69.99%) - LeetSync

---
+++ 125-valid-palindrome/valid-palindrome.py
@@ -0,0 +1,22 @@
+class Solution(object):
+    def isPalindrome(self, s):
+        """
+        :type s: str
+        :rtype: bool
+        """
+        l=0
+        r=len(s)-1
+        while l < r:
+            if not s[l].isalnum():
+                l+=1
+            elif not s[r].isalnum():
+                r-=1
+            elif s[l].lower() != s[r].lower():
+                return False
+            else:
+                l+=1
+                r-=1
+        return True
+        
+
+        
