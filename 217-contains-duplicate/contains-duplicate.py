a56027306fdc2bffb118db2dc8534caa3786597a
Samiksha Dnyanoba Poul <poulsamiksha7@gmail.com>
2025-10-04 01:35:08 +0530

    Time: 11 ms (89.35%) | Memory: 24.5 MB (88.45%) - LeetSync

---
+++ 217-contains-duplicate/contains-duplicate.py
@@ -0,0 +1,18 @@
+class Solution(object):
+    def containsDuplicate(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: bool
+        """
+        count={}
+        for i in nums:
+            if i not in count:
+                count[i]=1
+
+            else:
+                return True
+        return False
+
+        
+
+            
