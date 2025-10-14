2cff361ad41f364985d61b4595d2f6a221a86b3e
Samiksha Dnyanoba Poul <poulsamiksha7@gmail.com>
2025-10-14 22:11:07 +0530

    Time: 17 ms (20.40%) | Memory: 13.7 MB (33.10%) - LeetSync

--- 169-majority-element/majority-element.py
+++ 169-majority-element/majority-element.py
@@ -5,17 +5,11 @@ class Solution(object):
         :rtype: int
         """
         n=len(nums)
-        count=0
-        max_no=0
-        for i in range(0,n):
-            if count==0:
-                max_no=nums[i]
-                count=1
-            elif max_no==nums[i]:
-                count+=1
-            else:
-                count-=1
-        return max_no
+        count={}
+        for i in nums:
+            count[i]=count.get(i,0)+1
+            if count[i]>n//2:
+                return i
                   
             
      
