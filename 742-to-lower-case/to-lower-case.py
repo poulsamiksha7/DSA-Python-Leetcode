39a1bf73b97d2400eb1f158cb04b25702b1907ae
Samiksha Dnyanoba Poul <poulsamiksha7@gmail.com>
2025-11-20 15:31:02 +0530

    Time: 0 ms (100.00%) | Memory: 12.5 MB (10.59%) - LeetSync

--- 742-to-lower-case/to-lower-case.py
+++ 742-to-lower-case/to-lower-case.py
@@ -4,4 +4,10 @@ class Solution(object):
         :type s: str
         :rtype: str
         """
-        return s.lower()
+        ans=""
+        for ch in s:
+            if 'A' <= ch <= 'Z':
+                ans+=chr(ord(ch)+32)
+            else:
+                ans+=ch
+        return ans
