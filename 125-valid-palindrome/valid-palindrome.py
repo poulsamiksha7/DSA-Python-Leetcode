270d26a4416698ca88a7d00304c8209dc6639a25
Samiksha Dnyanoba Poul <poulsamiksha7@gmail.com>
2025-11-29 23:21:43 +0530

    Time: 135 ms (24.41%) | Memory: 13.1 MB (59.66%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.py
+++ 125-valid-palindrome/valid-palindrome.py
@@ -4,19 +4,9 @@ class Solution(object):
         :type s: str
         :rtype: bool
         """
-        l=0
-        r=len(s)-1
-        while l < r:
-            if not s[l].isalnum():
-                l+=1
-            elif not s[r].isalnum():
-                r-=1
-            elif s[l].lower() != s[r].lower():
-                return False
-            else:
-                l+=1
-                r-=1
-        return True
-        
-
-        
+        a=""
+        for i in s:
+            if i.isalnum():
+                a+=i.lower()
+        return a==a[::-1]
+                
