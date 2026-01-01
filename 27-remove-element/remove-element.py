766fa60395f900ce9f4febf708a90eb75f6818c3
Samiksha Dnyanoba Poul <poulsamiksha7@gmail.com>
2026-01-01 22:46:32 +0530

    Time: 0 ms (100.00%) | Memory: 12.5 MB (11.13%) - LeetSync

--- 27-remove-element/remove-element.py
+++ 27-remove-element/remove-element.py
@@ -5,11 +5,9 @@ class Solution(object):
         :type val: int
         :rtype: int
         """
-        k=0
-        n=len(nums)
-        for i in range(0,n):
-            if nums[i]!=val:
-                nums[k]=nums[i]
-                k+=1
-        return k
-        
+        count=0
+        for i in range(len(nums)):
+                if nums[i]!=val:
+                    nums[count]=nums[i]
+                    count+=1
+        return count
