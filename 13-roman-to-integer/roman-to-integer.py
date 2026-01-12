e0bff8796bbb6d5105eca878938e5be14045a9b3
Samiksha Dnyanoba Poul <poulsamiksha7@gmail.com>
2026-01-12 22:05:43 +0530

    Time: 12 ms (38.26%) | Memory: 12.6 MB (16.91%) - LeetSync

--- 13-roman-to-integer/roman-to-integer.py
+++ 13-roman-to-integer/roman-to-integer.py
@@ -4,13 +4,19 @@ class Solution(object):
         :type s: str
         :rtype: int
         """
-        my_dict={"I":1,"V":5,"X":10,"L":50,"C":100,"D":500,"M":1000}
         result=0
-        for i in range(len(s)-1):
-            if my_dict[s[i]]<my_dict[s[i+1]]:
-                result-=my_dict[s[i]]
+        roman={
+            'I':1,
+            'V':5,
+            'X':10,
+            'L':50,
+            'C':100,
+            'D':500,
+            'M':1000
+        }
+        for a,b in zip(s,s[1:]):
+            if roman[a]<roman[b]:
+                result-=roman[a]
             else:
-                result+=my_dict[s[i]]
-        result+=my_dict[s[-1]]
-        return result
-        
+                result+=roman[a]
+        return result+roman[s[-1]]
